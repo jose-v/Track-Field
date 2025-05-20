1bbde05e87699fc903caf02087491343fa732636
JGV <user@Offices-iMac.local>
2025-05-20 15:40:42 -0400

    Add SponsorLogosSection to Home page layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,12 @@ import HeroSection from './HeroSection'
 import FeaturesSection from './FeaturesSection'
 import TestimonialsSection from './TestimonialsSection'
 import CTASection from './CTASection'
+import SponsorLogosSection from './SponsorLogosSection'
 
 const Home = () => (
   <>
     <HeroSection />
+    <SponsorLogosSection />
     <FeaturesSection />
     <TestimonialsSection />
     <CTASection />
