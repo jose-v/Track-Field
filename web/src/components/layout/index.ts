16e31373470d949bcad2e49d930eabb86ae097fd
Jose Vazquez <hello@josev.co>
2025-05-26 12:36:38 -0400

    Delete web directory (duplicate)

--- web/src/components/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { BaseLayout } from './BaseLayout';
-export { useCoachNavigation } from './CoachNavigation';
-export { useAthleteNavigation } from './AthleteNavigation'; 
--- web/src/scripts/run-populate-badges.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run Badge Population Script
-echo "Running badge population script..."
-npx ts-node src/scripts/populateBadges.ts 
